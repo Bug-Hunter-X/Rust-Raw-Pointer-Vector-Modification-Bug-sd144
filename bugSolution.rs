fade794c632334486594a4af11032c755b16627c
BugHunter-X <askzenai@gmail.com>
2025-02-05 10:38:04 +0100

    Add bugSolution.rs file

---
+++ bugSolution.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut v = vec![1, 2, 3];
+    let len = v.len();
+    let ptr = v.as_mut_ptr();
+    unsafe {
+        for i in 0..len {
+            *ptr.add(i) = (*ptr.add(i)) * 2; 
+        }
+    }
+    println!("{:?}", v);
+}
